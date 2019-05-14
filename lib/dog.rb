a0a2511af7fc495d1cee6f477c737bc6ee170f19
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-05-14 13:43:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,12 @@
 class Dog
   @@all = []
 
-  attr_accessor :name
+    attr_accessor :name
 
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
+    def initialize(name)
+      @name = name
+      @@all << self
+    end
 
   def clear_all
       @@all.clear
